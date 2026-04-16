7e3d4d377ae145ec3544a6ed21bc53239dd29b88
youssefelsheikh24 <youssefelsheikh24@gmail.com>
2026-04-16 19:44:59 +0200

    fix the changes

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.get('/', (req, res) => {
 console.log('Hello, World!');
 
 console.log('inshallah n3ad');
+console.log('testtestest');
 
 app.listen(4000, () => {
   console.log('Server is running on port 3000');
